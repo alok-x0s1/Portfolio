0902624e2c14ec3a053f22de93fb767e9375aa1d
Alok Yadav <yalok6321@gmail.com>
2024-07-29 21:47:48 +0530

    Adding menu for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { lobster } from "@/data/fonts";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
@@ -19,17 +19,33 @@ const Navbar = () => {
 		},
 	];
 
+	const handleResize = () => {
+		if (window.innerWidth > 768) {
+			setNav(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
-		<div className="flex justify-between items-center w-[75rem] p-4">
+		<div className="relative flex justify-between items-center w-[75rem] p-4">
+			{/* Logo Section */}
 			<div>
-				<h1 className={`${lobster.className} text-3xl font-semibold ml-2 hover:text-secondaryTextColor duration-200`}>
+				<h1
+					className={`${lobster.className} text-3xl font-semibold ml-2 hover:text-secondaryTextColor duration-200`}
+				>
 					<Link className="" href="/" rel="noreferrer">
 						<span>@lok</span>
 						<span className="text-secondaryTextColor">Ya∂av</span>
 					</Link>
 				</h1>
 			</div>
-
+			{/* Desktop Menu */}
 			<ul className="hidden md:flex md:items-center">
 				{links.map(({ id, link }) => (
 					<li
@@ -43,6 +59,47 @@ const Navbar = () => {
 					<Link href="/projects">Resume</Link>
 				</li>
 			</ul>
+			{/* Mobile Menu Button */}
+			<div
+				className="md:hidden absolute top-4 right-4 cursor-pointer z-50"
+				onClick={() => setNav(!nav)}
+			>
+				{nav ? (
+					<FaTimes className="text-2xl" />
+				) : (
+					<FaBars className="text-2xl" />
+				)}
+			</div>
+			{/* Mobile Menu */}
+			<div
+				className={`fixed top-0 right-0 h-full p-4 w-1/2 sm:w-1/3 transform z-10 ${
+					nav ? "translate-x-0" : "translate-x-full"
+				} transition-transform duration-300 ease-in-out origin-top-right backdrop-blur-md bg-white bg-opacity-30`}
+			>
+				<ul className="flex flex-col items-center mt-8">
+					<li
+						className="nav-links py-2 text-lg cursor-pointer"
+						onClick={() => setNav(false)}
+					>
+						<Link href="/">Home</Link>
+					</li>
+					{links.map(({ id, link }) => (
+						<li
+							key={id}
+							className="nav-links py-2 text-lg cursor-pointer capitalize"
+							onClick={() => setNav(false)}
+						>
+							<Link href={link}>{link}</Link>
+						</li>
+					))}
+					<li
+						className="nav-links py-2 text-lg cursor-pointer"
+						onClick={() => setNav(false)}
+					>
+						<Link href="/projects">Resume</Link>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
