1a9d8e5a5e672c4de31b2383ccd05977c317450d
Alok Yadav <yalok6321@gmail.com>
2024-11-11 18:32:05 +0530

    add a separate file for projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,196 @@
+const featuredProjects = [
+	{
+		stacks: [
+			"NextJs",
+			"MongoDB",
+			"TypeScript",
+			"Tailwind CSS",
+			"Framer Motion",
+		],
+		title: "ShrinkIt",
+		description:
+			"Developed a modern URL shortening application with advanced features including password protection, QR code generation, and link management. Implemented user authentication using built-in middleware and cookies. Utilized MongoDB with Mongoose for efficient data handling. Integrated TypeScript for improved type safety and maintainability.",
+		githubLink: "https://github.com/alok-x0s1/ShrinkIt",
+		liveLink: "https://shuck.vercel.app",
+	},
+	{
+		stacks: [
+			"NextJs",
+			"MongoDB",
+			"NextAuth",
+			"OpenAI",
+			"Typescript",
+			"Tailwind CSS",
+		],
+		title: "Flickr",
+		description:
+			"Flickr is an anonymous feedback app built with Next.js. Users can manage incoming messages through a dashboard, accepting or declining feedback. Share your profile link to receive anonymous messages. The app uses Tailwind CSS, Radix UI, NextAuth, and OpenAI for a secure and seamless experience.",
+		githubLink: "https://github.com/alok-x0s1/Flickr",
+		liveLink: "https://flickrrr.vercel.app",
+	},
+	{
+		stacks: [
+			"React",
+			"Firebase",
+			"Stripe",
+			"Redux",
+			"ThreeJs",
+			"Tailwind CSS",
+		],
+		title: "Arise",
+		description:
+			"A React-based e-commerce platform that offers an immersive shopping experience. The app uses Firebase for the backend, handling user authentication, data storage, and real-time updates. Stripe is integrated for secure payment processing, with Firebase Cloud Functions generating clientSecrets.",
+		githubLink: "https://github.com/alok-x0s1/Arise",
+		liveLink: "https://a-rise-sigma.vercel.app",
+	},
+];
+
+const frontendProjects = [
+	{
+		stacks: ["React.js", "Firebase", "Tailwind CSS", "Context API"],
+		title: "Recipe Wiz",
+		description:
+			"A dynamic and responsive recipe discovery app built with React.js. It allows users to search, filter, and explore a wide variety of recipes based on cuisine, meal type, time, difficulty, and more. Perfect for food enthusiasts who want to try new dishes or find something that fits their schedule and skills.",
+		githubLink: "https://github.com/alok-x0s1/Recipe-Wiz",
+		liveLink: "https://recipewiz.vercel.app",
+	},
+	{
+		stacks: ["React.js", "Javascript", "Hooks", "Tailwind CSS"],
+		title: "Password Generator",
+		description:
+			"Introducing a sleek React Password Generator leveraging useRef, useEffect, and useCallback hooks. It automatically generates strong passwords, updates in real-time, and offers a quick copy feature for seamless integration.",
+		githubLink: "https://github.com/alok-x0s1/Password-Generator",
+		liveLink: "https://password-generator-alok.vercel.app",
+	},
+
+	{
+		stacks: ["React.js", "Tailwind CSS", "Javascript"],
+		title: "Playlist Pro",
+		description:
+			"PlaylistPro is a solution for effortless playlist management. Input a playlist link to calculate total duration and video count. Customize filters for watching speeds from 0.25x to 2x and view video demos with thumbnails, durations, titles, and YouTube links. Enjoy efficient data handling, robust error management, and an intuitive user experience.",
+		githubLink: "https://github.com/alok-x0s1/PlaylistPro",
+		liveLink: "https://playlistpro-alok.vercel.app/",
+	},
+	{
+		stacks: ["React.js", "Javascript", "Tailwind CSS"],
+		title: "Currency Convertor",
+		description:
+			"Discover a sleek currency converter powered by Vite, boasting custom hooks for streamlined functionality. With an extensive array of currency options and real-time updates, it ensures accurate conversions on any device. Simplify international transactions with ease.",
+		githubLink: "https://github.com/alok-x0s1/Currency-Convertor",
+		liveLink: "https://currency-convertor-alok.vercel.app/",
+	},
+];
+
+const fullStackProjects = [
+	{
+		stacks: [
+			"Next.Js",
+			"Drizzle",
+			"PostgreSQL",
+			"Framer Motion",
+			"Tailwind CSS",
+		],
+		title: "Sumera",
+		description:
+			"A comprehensive finance management application designed to help users gain control over their finances by tracking expenses, setting budgets, and analyzing spending patterns. This project is designed to help you manage your finances effectively by tracking your expenses, setting budgets, and analyzing your spending habits.",
+		githubLink: "https://github.com/alok-x0s1/Sumera",
+		liveLink: "https://sumera.vercel.app",
+	},
+	{
+		stacks: ["Next.js", "TypeScript", "Tailwind CSS"],
+		title: "Harmony Hub",
+		description:
+			"Harmony Hub is a cutting-edge music academy app built with Next.js. It transforms music education by providing an intuitive platform for students, teachers, and administrators. With features for interactive lessons, performance tracking, and streamlined administration, Harmony Hub enhances the learning experience and simplifies school management.",
+		githubLink: "https://github.com/alok-x0s1/Harmony-Hub",
+		liveLink: "https://harmony-hub-alok.vercel.app/",
+	},
+	{
+		stacks: ["Vite", "Express.js", "MongoDB", "TypeScript", "Web Sockets"],
+		title: "Chat Nexus",
+		description:
+			"A real-time chatting application built using WebSocket. This project is divided into two main sections: the frontend and the backend. The frontend provides a user-friendly interface for creating chat rooms and sending messages, while the backend handles the real-time communication between multiple users.",
+		githubLink: "https://github.com/alok-x0s1/Learning-ws",
+	},
+];
+
+const otherProjects = [
+	{
+		stacks: ["Html", "CSS", "Javascript"],
+		title: "Quote Generator",
+		description:
+			"Random Quote Generator website! Powered by JavaScript, it offers a range of features to keep you inspired. With just a click, you can copy quotes to your clipboard, listen to them read aloud, or share them on Twitter. The 'Inspire Me' button lets you generate a new quote whenever you need a boost, and quotes automatically refresh every 15 seconds to ensure a steady flow of inspiration.",
+		githubLink: "https://github.com/alok-x0s1/Quote-Generator",
+		liveLink: "https://quote-generator-alok.vercel.app/",
+	},
+	{
+		stacks: ["Html", "CSS", "Javascript"],
+		title: "QrSwift",
+		description:
+			"This project lets you create QR codes using a web page. You enter some text or a URL, and the QR code is generated instantly. The project uses HTML for the structure of the page, CSS for styling it, and JavaScript to handle the QR code creation. It's a handy tool for quickly converting any information into a scannable QR code.",
+		githubLink: "https://github.com/alok-x0s1/QrSwift",
+		liveLink: "https://qrswift.vercel.app/",
+	},
+];
+
+const backendProjects = [
+	{
+		stacks: [
+			"Node.js",
+			"Express.js",
+			"MongoDB",
+			"Cloudinary",
+			"Multer",
+			"JWT",
+		],
+		title: "EchoTube",
+		description:
+			"EchoTube is a backend application that mimics YouTube’s core functionalities. It features user authentication, video management, likes, comments, and tweets. The app utilizes Node.js, Express.js, and MongoDB for efficient data handling. The project also includes cloud storage for video uploads and image thumbnails.",
+		githubLink: "https://github.com/alok-x0s1/EchoTube",
+	},
+	{
+		stacks: ["Express.js", "Prisma", "PostgreSQL", "Zod", "TypeScript"],
+		title: "Backend API for Task management",
+		description:
+			"A simple yet powerful backend for a task management system, built using Express, Prisma, PostgreSQL, and TypeScript. This project allows users to manage projects, assign tasks, and track progress, while ensuring proper role-based access control.",
+		githubLink: "https://github.com/alok-x0s1/Learning-prisma",
+	},
+];
+
+const techStacks = [
+	{
+		name: "Programming Languages",
+		skills: ["C++, ", "JavaScript, ", "TypeScript, ", "SQL"],
+	},
+	{
+		name: "Frontend",
+		skills: ["Next.js, ", "React, ", "Redux, ", "Tailwind CSS"],
+	},
+	{
+		name: "Backend",
+		skills: ["Node.js, ", "Express.js, ", "Appwrite, ", "Firebase"],
+	},
+	{
+		name: "Databases",
+		skills: ["MongoDB, ", "Mongoose, ", "PostgreSQL, ", "Prisma"],
+	},
+];
+
+const tools = [
+	"VsCode, ",
+	"Postman, ",
+	"Git, ",
+	"Github, ",
+	"Gitlab, ",
+	"Vercel, ",
+	"Netlify",
+];
+
+export {
+	featuredProjects,
+	frontendProjects,
+	fullStackProjects,
+	otherProjects,
+	backendProjects,
+	techStacks,
+	tools,
+};
