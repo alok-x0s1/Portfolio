8374d3ef0cdae40587e076ce49a2ae75bcb0572e
Alok Yadav <yalok6321@gmail.com>
2025-04-02 08:51:58 +0530

    add favicon and use a different font

--- src/data/fonts.ts
+++ src/data/fonts.ts
@@ -1,16 +1,13 @@
-import { Rubik, Lobster, Orbitron } from "next/font/google";
+import { Rubik } from "next/font/google";
+import localFont from "next/font/local";
 
 export const rubik = Rubik({
 	weight: ["400", "500", "600", "700"],
 	subsets: ["latin"],
+	variable: "--font-rubik",
 });
 
-export const orbitron = Orbitron({
-	weight: ["400", "500", "600", "700"],
-	subsets: ["latin"],
-});
-
-export const lobster = Lobster({
-	weight: ["400"],
-	subsets: ["latin"],
+export const calSans = localFont({
+	src: "../../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
 });
